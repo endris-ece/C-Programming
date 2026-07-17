70fb009c8a4f70eb0f893f5ae71fe0991a03f708
Endris Mohammed <endris.vision.02@gmail.com>
2026-07-17 10:41:00 +0300

    Add format specifier

--- 01_Basics/format_specifier.c
+++ 01_Basics/format_specifier.c
@@ -8,6 +8,7 @@ int main(void){
     double pi = 3.14;
     char name[] = "Endris";
 
+    printf("My name is %s.\n", name);
     printf("I am %d years old.\n", age);
     printf("Voltage = %f\n", voltage);
     printf("My physics grade is %c\n", grade);
